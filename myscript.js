d2f34d26fc704f312656b4e04dea10d8170653d5
Nathan Petts <nbpetts@gmail.com>
2016-05-09 12:19:18 -0500

    still fixing skip link

--- myscript.js
+++ myscript.js
@@ -1,8 +1,12 @@
 "use strict";
 
-$('details').details();
+$(document).ready(function() {
+  $('details').details(); // for firefox polyfill
 
-// thank you Martijn van der Ven https://gist.github.com/Zegnat
-$("a[href^='#']").not("a[href='#']").click(function() {
-   $("#"+$(this).attr("href").slice(1)+"").focus();
-});
+  // thank you Martijn van der Ven https://gist.github.com/Zegnat
+  $("a[href^='#']").not("a[href='#']").click(function() {
+     $("#"+$(this).attr("href").slice(1)+"").focus();
+  });
+
+
+});// end ready
