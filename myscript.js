e4b3b5533c8be08ac1eaa02ee4c567f2ae5c5248
Nathan Petts <nbpetts@gmail.com>
2016-05-09 11:53:14 -0500

    javascript fix for webkit browsers and skip links.

--- myscript.js
+++ myscript.js
@@ -1,3 +1,8 @@
 "use strict";
 
 $('details').details();
+
+// thank you Martijn van der Ven https://gist.github.com/Zegnat
+$("a[href^='#']").not("a[href='#']").click(function() {
+   $("#"+$(this).attr("href").slice(1)+"").focus();
+});
